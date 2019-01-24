2dced25f661b52bb53fe182230abd2fa366f85b1
HE Boliang <hebl@hebl.org>
2019-01-24 22:37:33 +0800

    update pg to v7

--- xweb/pg.go
+++ xweb/pg.go
@@ -1,12 +1,15 @@
 package xweb
 
-import "gopkg.in/pg.v3"
+import (
+	"fmt"
+
+	"github.com/go-pg/pg"
+)
 
 // NewPg 新建一个PostgreSQL连接
 func NewPg(dbconfig DBConfig) *pg.DB {
 	return pg.Connect(&pg.Options{
-		Host:     dbconfig.Host,
-		Port:     dbconfig.Port,
+		Addr:     fmt.Sprintf("%s:%s", dbconfig.Host, dbconfig.Port),
 		Database: dbconfig.Database,
 		User:     dbconfig.User,
 		Password: dbconfig.Password,
